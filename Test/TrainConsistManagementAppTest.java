dd8444003eecf8dc9b4a1db598335bcf6c1a617b
balatharunr <r.balatharun@gmail.com>
2026-04-09 10:05:23 +0530

    uc8 pending

--- Test/TrainConsistManagementAppTest.java
+++ Test/TrainConsistManagementAppTest.java
@@ -1,5 +1,43 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrainConsistManagementAppTest {
+    @Test
+    void testFilter_CapacityGreaterThanThreshold() {
 
+    }
+
+    @Test
+    void testFilter_CapacityEqualToThreshold() {
+
+    }
+
+    @Test
+    void testFilter_CapacityLessThanThreshold() {
+
+    }
+
+    @Test
+    void testFilter_MultipleBogiesMatching() {
+
+    }
+
+    @Test
+    void testFilter_NoBogiesMatching() {
+
+    }
+
+    @Test
+    void testFilter_AllBogiesMatching() {
+
+    }
+
+    @Test
+    void testFilter_EmptyBogieList() {
+
+    }
+
+    @Test
+    void testFilter_OriginalListUnchanged() {
+
+    }
 }
