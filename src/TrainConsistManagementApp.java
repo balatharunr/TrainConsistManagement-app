c0fa27f0dda4bd45d2d7ed332b53efc5e880d127
balatharunr <r.balatharun@gmail.com>
2026-04-09 10:35:54 +0530

    UC8

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TrainConsistManagementApp {
-    static class Bogie{
+    static class Bogie {
         String name;
         int capacity;
         Bogie(String name, int capacity) {
@@ -15,28 +13,29 @@ public class TrainConsistManagementApp {
             return name + " -> " + capacity;
         }
     }
+    public static List<Bogie> filterBogies(List<Bogie> bogies, int threshold) {
+        return bogies.stream()
+                .filter(b -> b.capacity > threshold)
+                .toList();
+    }
     public static void main(String[] args) {
         System.out.println("============================\n");
-        System.out.println("Filter Passenger Bogies Using Streams");
+        System.out.println("UC8 - Filter Passenger Bogies Using Streams");
         System.out.println("============================\n");
         List<Bogie> bogies = new ArrayList<>();
-        Map<String, Integer> capacityMap = new HashMap<>();
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
         bogies.add(new Bogie("General", 90));
-        System.out.println("All Bogies:\n");
+        System.out.println("ALL Bogies:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
-        System.out.println("Filtered bogies (Capacity > 60):\n");
-        for (Bogie b : bogies) {
+        List<Bogie> filtered = filterBogies(bogies, 60);
+        System.out.println("\nFiltered Bogies (Capacity > 60):");
+        for (Bogie b : filtered) {
             System.out.println(b);
         }
-        System.out.println("\nAfter Sorting by Capacity:\n");
-        bogies.sort((b1, b2) -> b1.capacity - b2.capacity);
-        for (Bogie b : bogies) {
-            System.out.println(b);
-        }
+        System.out.println("\nUC8 filtering completed...");
     }
 }
