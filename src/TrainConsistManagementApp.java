d767c0537819075cc471f82cfd7d7e4923be8898
balatharunr <r.balatharun@gmail.com>
2026-04-15 23:19:04 +0530

    modify uc8

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,55 +1,38 @@
-import java.util.*;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class TrainConsistManagementApp {
-
-    static class GoodsBogie {
-        String type;
-        String cargo;
-
-        GoodsBogie(String type, String cargo) {
-            this.type = type;
-            this.cargo = cargo;
+    static class Bogie{
+        String name;
+        int capacity;
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
         }
-
         public String toString() {
-            return type + " -> " + cargo;
+            return name + " -> " + capacity;
         }
     }
-    public static boolean isTrainSafe(List<GoodsBogie> bogies) {
-        return bogies.stream()
-                .allMatch(b ->
-                        !b.type.equalsIgnoreCase("Cylindrical")
-                                || b.cargo.equalsIgnoreCase("Petroleum")
-                );
-    }
-
     public static void main(String[] args) {
-
         System.out.println("============================\n");
-        System.out.println("UC12 - Safety Compliance Check for Goods Bogies");
+        System.out.println("Filter Passenger Bogies Using Streams");
         System.out.println("============================\n");
-
-        List<GoodsBogie> goodsBogies = new ArrayList<>();
-
-        goodsBogies.add(new GoodsBogie("Cylindrical", "Petroleum"));
-        goodsBogies.add(new GoodsBogie("Open", "Coal"));
-        goodsBogies.add(new GoodsBogie("Box", "Grain"));
-        goodsBogies.add(new GoodsBogie("Cylindrical", "Coal"));
-
-        System.out.println("Goods Bogies in Train:");
-        for (GoodsBogie b : goodsBogies) {
+        List<Bogie> bogies = new ArrayList<>();
+        Map<String, Integer> capacityMap = new HashMap<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("General", 90));
+        System.out.println("Before Sorting:\n");
+        for (Bogie b : bogies) {
             System.out.println(b);
         }
-        boolean isSafe = isTrainSafe(goodsBogies);
-
-        System.out.println("\nSafety Compliance Status: " + isSafe);
-
-        if (isSafe) {
-            System.out.println("Train formation is SAFE.");
-        } else {
-            System.out.println("Train formation is NOT SAFE.");
+        System.out.println("\nAfter Sorting by Capacity:\n");
+        bogies.sort((b1, b2) -> b1.capacity - b2.capacity);
+        for (Bogie b : bogies) {
+            System.out.println(b);
         }
-
-        System.out.println("\nUC12 safety validation completed...");
     }
 }
